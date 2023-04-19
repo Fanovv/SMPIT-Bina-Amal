95cf8c1a2b257982d5cd6ce2f63a8f3bc9d2bd0c
Fanovv <affannovian@gmail.com>
2023-04-19 19:44:19 +0700

    test push laptop

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>{{ config('name', 'Login User') }}</title>
+    <title>{{ config('name', 'Bina Amal GunungPati') }}</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
